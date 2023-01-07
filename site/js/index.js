ed10b27043fead67a59d3768c25455200dd0d540
Barbér Máté <mate1024@gmail.com>
2023-01-07 22:38:43 +0100

    Fix mobile menu

--- site/js/index.js
+++ site/js/index.js
@@ -1,10 +1,12 @@
 window.onload = () => {
-	document.getElementById("hamburger").onclick = () => {
-		if (document.getElementById("mobile-menu") && document.getElementById("mobile-menu").style.height === "0px") {
-			document.getElementById("mobile-menu").style.height = "auto"
-		}
-		if (document.getElementById("mobile-menu") && document.getElementById("mobile-menu").style.height === "auto") {
-			document.getElementById("mobile-menu").style.height = 0
+	if (document.getElementById("hamburger")) {
+		document.getElementById("hamburger").onclick = () => {
+			if (document.getElementById("mobile-menu") && document.getElementById("mobile-menu").style.maxHeight === "0px") {
+				document.getElementById("mobile-menu").style.maxHeight = "197.5px"
+			}
+			else if (document.getElementById("mobile-menu") && document.getElementById("mobile-menu").style.maxHeight === "197.5px") {
+				document.getElementById("mobile-menu").style.maxHeight = 0
+			}
 		}
 	}
 }
